8605dfde09015c436a9642da3e9f38b1cb443ef1
thanhthai_nguyen <17521031@gm.uit.edu.vn>
2020-04-27 18:30:45 +0700

    amadeus shop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-require('dotenv').config()
-
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
@@ -7,7 +5,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import NavigationURL from "./router/NavigationURL";
-
+require('dotenv').config()
 function App() {
   return (
     <div className="App">
