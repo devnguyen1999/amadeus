e6753ab98a49f86cb866762d36e94e4ea2fc4c8f
Trương Pháp <truongphapdh@gmail.com>
2020-06-24 09:13:12 +0700

    update image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,84 +4,82 @@ import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
 import Data from "../product-data.json";
 import "./Home.css";
-import axios from 'axios';
+import axios from "axios";
 
-
-
-
-export default class Home extends React.Component{
+export default class Home extends React.Component {
   state = {
-    ListData: []
-    
-  }
-  
+    ListData: [],
+  };
+
   componentDidMount() {
-    axios.get(`https://amadeuss.herokuapp.com/products`)
+    axios
+      .get(`https://amadeuss.herokuapp.com/products`)
       .then(res => {
         const ListData = res.data;
         this.setState({ ListData });
-      
       })
       .catch(error => console.log(error));
   }
 
-
-
-render(){
-  return (
-    <div>
-      <div id="carouselId" className="carousel slide" data-ride="carousel">
-        <ol className="carousel-indicators">
-          <li data-target="#carouselId" data-slide-to={0} className="active" />
-          <li data-target="#carouselId" data-slide-to={1} />
-          <li data-target="#carouselId" data-slide-to={2} />
-        </ol>
-        <div className="carousel-inner" role="listbox">
-          <div className="carousel-item active">
-            <img
-              src="https://compass-ssl.xbox.com/assets/05/75/0575153b-9efa-4620-8afe-b6980200f60f.jpg?n=215489_Page-Hero-1084_1920x720.jpg"
-              className="img-fluid"
-              alt="First slide"
+  render() {
+    return (
+      <div>
+        <div id="carouselId" className="carousel slide" data-ride="carousel">
+          <ol className="carousel-indicators">
+            <li
+              data-target="#carouselId"
+              data-slide-to={0}
+              className="active"
             />
+            <li data-target="#carouselId" data-slide-to={1} />
+            <li data-target="#carouselId" data-slide-to={2} />
+          </ol>
+          <div className="carousel-inner" role="listbox">
+            <div className="carousel-item active">
+              <img
+                src="https://compass-ssl.xbox.com/assets/05/75/0575153b-9efa-4620-8afe-b6980200f60f.jpg?n=215489_Page-Hero-1084_1920x720.jpeg"
+                className="img-fluid"
+                alt="First slide"
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://i.imgur.com/6mjaYdD.jpeg"
+                className="img-fluid"
+                alt="Second slide"
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://i.imgur.com/R095ZFO.jpeg"
+                className="img-fluid"
+                alt="Third slide"
+              />
+            </div>
           </div>
-          <div className="carousel-item">
-            <img
-              src="https://i.imgur.com/6mjaYdD.png"
-              className="img-fluid"
-              alt="Second slide"
-            />
-          </div>
-          <div className="carousel-item">
-            <img
-              src="https://i.imgur.com/R095ZFO.jpg"
-              className="img-fluid"
-              alt="Third slide"
-            />
-          </div>
-        </div>
-        <a
-          className="carousel-control-prev"
-          href="#carouselId"
-          role="button"
-          data-slide="prev"
-        >
-          <span className="carousel-control-prev-icon" aria-hidden="true" />
-          <span className="sr-only">Previous</span>
-        </a>
-        <a
-          className="carousel-control-next"
-          href="#carouselId"
-          role="button"
-          data-slide="next"
-        >
-          <span className="carousel-control-next-icon" aria-hidden="true" />
-          <span className="sr-only">Next</span>
-        </a>
-      </div>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-12 col-md-3">
           <a
+            className="carousel-control-prev"
+            href="#carouselId"
+            role="button"
+            data-slide="prev"
+          >
+            <span className="carousel-control-prev-icon" aria-hidden="true" />
+            <span className="sr-only">Previous</span>
+          </a>
+          <a
+            className="carousel-control-next"
+            href="#carouselId"
+            role="button"
+            data-slide="next"
+          >
+            <span className="carousel-control-next-icon" aria-hidden="true" />
+            <span className="sr-only">Next</span>
+          </a>
+        </div>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12 col-md-3">
+              <a
                 className="btn d-block text-white dropdown-toggle"
                 data-toggle="collapse"
                 href="#sortMenu"
@@ -142,74 +140,70 @@ render(){
                 aria-expanded="false"
                 aria-controls="filterMenu"
               >
-                <h4>
-                  Bộ lọc sản phẩm
-                </h4>
+                <h4>Bộ lọc sản phẩm</h4>
                 <hr className="border-white mt-2 mb-0" />
               </a>
               <div className="collapse" id="filterMenu"></div>
-          </div>
-          <div className="col-12 col-md-9">
-            <div className="row">
-              {this.state.ListData.map((value, key) => {
-    
-                return (
-                  <Product
-                    key={key}
-                    prID={value.id}
-                    prImg={value.img}
-                    prURL={value.nameURL}
-                    prTitle={value.name}
-                    prPrice={value.price} 
-                  />
-                );
-              
-              })}
+            </div>
+            <div className="col-12 col-md-9">
+              <div className="row">
+                {this.state.ListData.map((value, key) => {
+                  return (
+                    <Product
+                      key={key}
+                      prID={value.id}
+                      prImg={value.img}
+                      prURL={value.nameURL}
+                      prTitle={value.name}
+                      prPrice={value.price}
+                    />
+                  );
+                })}
+              </div>
             </div>
           </div>
         </div>
+        <nav aria-label="Page navigation example">
+          <ul className="pagination justify-content-center py-3">
+            <li className="page-item disabled">
+              <Link className="page-link" to="" tabIndex={-1}>
+                Trang trước
+              </Link>
+            </li>
+            <li className="page-item">
+              <Link className="page-link" to="">
+                1
+              </Link>
+            </li>
+            <li className="page-item">
+              <Link className="page-link" to="">
+                2
+              </Link>
+            </li>
+            <li className="page-item">
+              <Link className="page-link" to="">
+                3
+              </Link>
+            </li>
+            <li className="page-item">
+              <Link className="page-link" to="">
+                4
+              </Link>
+            </li>
+            <li className="page-item">
+              <Link className="page-link" to="">
+                5
+              </Link>
+            </li>
+            <li className="page-item">
+              <Link className="page-link" to="" tabIndex={+1}>
+                Trang sau
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <Footer />
       </div>
-      <nav aria-label="Page navigation example">
-        <ul className="pagination justify-content-center py-3">
-          <li className="page-item disabled">
-            <Link className="page-link" to="" tabIndex={-1}>
-              Trang trước
-            </Link>
-          </li>
-          <li className="page-item">
-            <Link className="page-link" to="">
-              1
-            </Link>
-          </li>
-          <li className="page-item">
-            <Link className="page-link" to="">
-              2
-            </Link>
-          </li>
-          <li className="page-item">
-            <Link className="page-link" to="">
-              3
-            </Link>
-          </li>
-          <li className="page-item">
-            <Link className="page-link" to="">
-              4
-            </Link>
-          </li>
-          <li className="page-item">
-            <Link className="page-link" to="">
-              5
-            </Link>
-          </li>
-          <li className="page-item">
-            <Link className="page-link" to="" tabIndex={+1}>
-              Trang sau
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <Footer/>
-    </div>
-  );
+    );
+  }
 }
- }
