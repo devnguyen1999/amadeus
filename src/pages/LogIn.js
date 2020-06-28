0bb4c10e038a34034061494e7d0a7b041c9bb2e1
unknown <thanhdevilll@gmail.com>
2020-06-28 20:35:25 +0700

    Thanh update 28/6/2020

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,29 +1,34 @@
 import "./LogIn.css";
 import React, { useState } from "react";
+import Header from "../components/Header";
 import axios from "axios";
-import { setUserSession, getUser } from "../Utils/Common";
-import { useHistory, Redirect } from "react-router-dom";
+import { setUserSession } from "../Utils/Common";
+import { Redirect } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
-function Login(props) {
+function LogIn(props) {
+  const { handleSubmit, register, errors } = useForm();
   const [loading, setLoading] = useState(false);
-  const email = useFormInput();
-  const password = useFormInput();
   const [error, setError] = useState(null);
-  const history = useHistory();
 
-  // handle button click of login form
-  const handleLogin = () => {
+  const onSubmit = (values) => {
     setError(null);
     setLoading(true);
     axios
       .post("https://amadeuss.herokuapp.com/api/auth/login", {
-        email: email.value,
-        password: password.value,
+        email: values.email,
+        password: values.password,
       })
       .then((response) => {
         setLoading(false);
         setUserSession(response.data.accessToken, response.data.user);
-        history.push("/");
+        return (
+          <Redirect
+            to={{
+              pathname: "/",
+            }}
+          />
+        );
       })
       .catch((error) => {
         setLoading(false);
@@ -31,34 +36,61 @@ function Login(props) {
           setError(error.response.data.message);
         else setError("Something went wrong. Please try again later.");
       });
+    console.log(values);
   };
 
   return (
     <div>
+      <Header />
       <div className="background">
         <img
           src="https://news.gearvn.com/wp-content/uploads/2019/12/Call-of-Duty-Modern-Warfare-Microtransactions.jpg"
           alt="Background"
         />
       </div>
-      <div className="form-group">
+      <form onSubmit={handleSubmit(onSubmit)} className="form-group">
         <input
-          type="email"
-          {...email}
+          name="email"
+          ref={register({
+            required: "Email không được để trống",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+              message: "Email không hợp lệ",
+            },
+          })}
           className="form-control"
           placeholder="Email đăng nhập"
+          type="email"
         />
+        {errors.email && (
+          <>
+            <span className="error">{errors.email.message}</span>
+          </>
+        )}
+
         <input
+          name="password"
+          ref={register({
+            required: "Mật khẩu không được để trống",
+            minLength: {
+              value: 6,
+              message: "Mật khẩu phải có ít nhất 6 kí tự",
+            },
+          })}
           type="password"
-          {...password}
           className="form-control mt-4"
           placeholder="Mật khẩu"
         />
+        {errors.password && (
+          <>
+            <span className="error">{errors.password.message}</span>
+          </>
+        )}
+
         <input
-          type="button"
-          className="btn btn-primary mt-4 mb-3 d-block"
+          type="submit"
+          className="btn btn-primary mt-4 d-block"
           value={loading ? "Loading..." : "Đăng nhập"}
-          onClick={handleLogin}
           disabled={loading}
         />
         {error && (
@@ -66,21 +98,9 @@ function Login(props) {
             <span className="error">{error}</span>
           </>
         )}
-      </div>
+      </form>
     </div>
   );
 }
 
-const useFormInput = (initialValue) => {
-  const [value, setValue] = useState(initialValue);
-
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  };
-  return {
-    value,
-    onChange: handleChange,
-  };
-};
-
-export default Login;
+export default LogIn;
