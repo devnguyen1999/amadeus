9a09b3be0f1221ed12ba3b7a967381b3c440de78
Trương Pháp <truongphapdh@gmail.com>
2020-07-07 23:21:28 +0700

    hight pagespeed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
-import "./App.css";
+//import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import NavigationURL from "./router/NavigationURL";
 import ScrollToTop from "./router/ScrollToTop";
@@ -9,8 +9,8 @@ function App() {
   const [count, setCount] = useState(0);
   setTimeout(() => {
     setCount(1);
-  }, 3000);
-  if (count === 1)
+  }, 2000);
+  if (count === 1) {
     return (
       <div className="App">
         <Router>
@@ -19,6 +19,7 @@ function App() {
         </Router>
       </div>
     );
+  }
   return <div>LOADING....</div>;
 }
 export default App;
