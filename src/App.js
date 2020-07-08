8d68e5776f9daf605fae11f15fe56f031d16bb05
Trương Pháp <truongphapdh@gmail.com>
2020-07-08 09:13:45 +0700

    hight speed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [count, setCount] = useState(0);
   setTimeout(() => {
     setCount(1);
-  }, 0);
+  }, 2000);
   if (count === 1) {
     return (
       <div className="App">
