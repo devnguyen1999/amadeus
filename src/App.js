655a9cd0d57ed3d379f15c52174d7864340b0fec
thanhthai_nguyen <17521031@gm.uit.edu.vn>
2020-07-10 10:59:56 +0700

    updated

--- src/App.js
+++ src/App.js
@@ -5,27 +5,11 @@ import NavigationURL from "./router/NavigationURL";
 import ScrollToTop from "./router/ScrollToTop";
 
 function App() {
-<<<<<<< HEAD
-
-=======
->>>>>>> cc07ca88cef069c0de8ccae7f69d3104a0b9c99d
   const [count, setCount] = useState(0);
   setTimeout(() => {
     setCount(1);
   }, 2000);
   if (count === 1) {
-<<<<<<< HEAD
-=======
-    return (
-      <div className="App">
-        <Router>
-          <ScrollToTop />
-          <NavigationURL />
-        </Router>
-      </div>
-    );
-  }
->>>>>>> cc07ca88cef069c0de8ccae7f69d3104a0b9c99d
   return (
     <div class="showbox">
       <div class="loader">
@@ -43,10 +27,7 @@ function App() {
       </div>
     </div>
   );
-<<<<<<< HEAD
   }
   return <div>LOADING....</div>;
-=======
->>>>>>> cc07ca88cef069c0de8ccae7f69d3104a0b9c99d
 }
 export default App;
