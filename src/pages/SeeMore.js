a67d6e941f07998245abd3435505c3e9cd7b274a
unknown <thanhdevilll@gmail.com>
2020-07-14 02:50:06 +0700

    Thanh update

--- src/pages/SeeMore.js
+++ src/pages/SeeMore.js
@@ -3,36 +3,6 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import axios from "axios";
 import ProductBlock from "../components/ProductBlock";
-import { useParams } from "react-router-dom";
-
-const toSlug = (str) => {
-  // Chuyển hết sang chữ thường
-  str = str.toLowerCase();
-
-  // xóa dấu
-  str = str.replace(/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/g, "a");
-  str = str.replace(/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/g, "e");
-  str = str.replace(/(ì|í|ị|ỉ|ĩ)/g, "i");
-  str = str.replace(/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/g, "o");
-  str = str.replace(/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/g, "u");
-  str = str.replace(/(ỳ|ý|ỵ|ỷ|ỹ)/g, "y");
-  str = str.replace(/(đ)/g, "d");
-
-  // Xóa ký tự đặc biệt
-  str = str.replace(/([^0-9a-z-\s])/g, "");
-
-  // Xóa khoảng trắng thay bằng ký tự -
-  str = str.replace(/(\s+)/g, "-");
-
-  // xóa phần dự - ở đầu
-  str = str.replace(/^-+/g, "");
-
-  // xóa phần dư - ở cuối
-  str = str.replace(/-+$/g, "");
-
-  // return
-  return str;
-};
 
 function SeeMore(props) {
   const [stt, setStt] = useState(0);
@@ -46,7 +16,7 @@ function SeeMore(props) {
         let arrPr = [];
         products.data.map((value, key) => {
           if (value.priceFake !== 0) {
-            arrPr[key] = value;
+            arrPr.push(value);
           }
         });
         setProducts(arrPr);
@@ -72,8 +42,6 @@ function SeeMore(props) {
       handled[j + 1] = temp;
     }
     setProducts(handled);
-    const arr = handled;
-    setProducts(arr);
     setStt(1);
   };
   const decrease = () => {
@@ -87,8 +55,7 @@ function SeeMore(props) {
       }
       handled[j + 1] = temp;
     }
-    const arr = handled;
-    setProducts(arr);
+    setProducts(handled);
     setStt(2);
   };
 
