fc276f892f8c70de08b7ff2f38dbd85fe07e6874
Trương Pháp <truongphapdh@gmail.com>
2020-07-14 00:40:41 +0700

    update

--- server.js
+++ server.js
@@ -11,9 +11,6 @@ app.use(compression());
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
-app.get("/ping", function (req, res) {
-  return res.send("pong");
-});
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
