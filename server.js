b6901dda5ea85485382c6b7878ebddb39758d1b0
thanhthai_nguyen <17521031@gm.uit.edu.vn>
2020-07-14 00:30:44 +0700

    updated

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 var compression = require("compression");
 const express = require("express");
-const favicon = require("express-favicon");
+//const favicon = require("express-favicon");
 const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(compression());
 
-app.use(favicon(__dirname + "/app/build/favicon.ico"));
+//app.use(favicon(__dirname + "/app/build/favicon.ico"));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
