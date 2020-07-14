094a58bfc3b46c05013711524c68d760d054b59e
Trương Pháp <truongphapdh@gmail.com>
2020-07-14 07:48:45 +0700

    update

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 require("dotenv").config();
 var compression = require("compression");
 const express = require("express");
-<<<<<<< HEAD
 //const favicon = require("express-favicon");  <======
-=======
-//const favicon = require("express-favicon");
->>>>>>> a67d6e941f07998245abd3435505c3e9cd7b274a
 const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(compression());
 
-<<<<<<< HEAD
 //app.use(favicon(__dirname + "/build/favicon.ico")); <========
-=======
-//app.use(favicon(__dirname + "/app/build/favicon.ico"));
->>>>>>> a67d6e941f07998245abd3435505c3e9cd7b274a
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
